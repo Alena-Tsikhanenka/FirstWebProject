901111573ca2a1f07de1a96f71e45ead63e0c322
Alena Tsikhanenka <alena.tsikhanenka@gmail.com>
2016-11-23 03:08:47 +0300

    Create MainServlet, update pom, set up ignored files

---
+++ src/main/java/com/gmail/alena/servlet/MainServlet.java
@@ -0,0 +1,42 @@
+package com.gmail.alena.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class MainServlet  extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MainServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		PrintWriter writer = response.getWriter();
+		writer.append("Served at: ").append(request.getContextPath());
+		writer.write("<div style='color:red'>Hello World!</div>");
+		writer.close();
+		*/
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}

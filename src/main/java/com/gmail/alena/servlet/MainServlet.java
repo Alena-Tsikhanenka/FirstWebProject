d11a2c396c1acc40247f4bb05904eecedb165224
Alena Tsikhanenka <alena.tsikhanenka@gmail.com>
2016-11-24 17:04:30 +0300

    Fix css and js errors, map MainServlet to root path only

--- src/main/java/com/gmail/alena/servlet/MainServlet.java
+++ src/main/java/com/gmail/alena/servlet/MainServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.gmail.alena.util.Constants;
 
-@WebServlet("/")
+@WebServlet("")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,3 +1,3 @@
-$(document).load(function() {
+$(document).ready(function() {
 	console.log('Hello, JavaScript!');
 });
